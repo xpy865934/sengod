520cc19d116356bdc01b58db1e5fae601b5863fb
xpy865934 <865934097@qq.com>
2018-01-14 22:21:22 +0800

    1、完成开始页面布局 2、完成主页布局 3、完成第三方蓝牙库的添加

---
+++ app/src/main/java/com/sengod/sengod/MyApplication.java
@@ -0,0 +1,19 @@
+package com.sengod.sengod;
+
+import android.app.Application;
+
+import com.inuker.bluetooth.library.BluetoothClient;
+
+/**
+ * Created by Lenovo on 2018/1/14.
+ */
+
+public class MyApplication extends Application{
+    public static BluetoothClient mBluetoothClient;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mBluetoothClient = new BluetoothClient(this);
+    }
+}
